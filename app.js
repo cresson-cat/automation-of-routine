3ec6933f407076e31a292a72685f6456b1eb8918
mellow.k <simple_life_dev@yahoo.co.jp>
2018-07-04 01:04:40 +0900

    コメント修正

--- app.js
+++ app.js
@@ -7,11 +7,9 @@ process.on('uncaughtException', function (err) {
     console.log(err);
 });
 
-/* todo：init.jsonから初期データを取得する（基本稼働時間や、週次の想定残業時間）
- * 引数が無かった場合は、以下変数にセットする */
-
 const userId = process.argv[2];   // 引数.. ユーザ名
 const password = process.argv[3]; // 引数.. パスワード
+// init.jsonから初期データを取得する（基本稼働時間や、週次の想定残業時間）
 const conf = JSON.parse(fs.readFileSync('./init.json', 'utf8')); // 初期設定ファイル取得
 
 // ユーザIDやパスワードが未設定の場合、処理終了
