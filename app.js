5a2f75f4837e82ce950f886dff54f13111653c85
D--A--I <kanegadai@kanegadainoMacBook-Pro.local>
2019-03-30 19:39:55 +0900

    一旦スケジュール実行機能を追加

--- app.js
+++ app.js
@@ -8,36 +8,36 @@ process.on('uncaughtException', function (err) {
     console.log(err);
 });
 
-/* ユーザ名／パスワード */
-const userId = process.argv[2];
-const password = process.argv[3];
-// DropBoxへのファイル保存
-const isSaved = !!process.argv[4] || false;
+/* メイン処理 */
+const main = function () {
 
-// ユーザIDやパスワードが未設定の場合、処理終了
-if ((userId === null || userId === undefined) || (password === null || password === undefined))
-    process.exit(0);
+    // init.jsonから初期データを取得する（基本稼働時間や、週次の想定残業時間等）
+    const conf = JSON.parse(fs.readFileSync('./init.json', 'utf8'));
 
-// init.jsonから初期データを取得する（基本稼働時間や、週次の想定残業時間等）
-const conf = JSON.parse(fs.readFileSync('./init.json', 'utf8'));
+    // ユーザIDやパスワードが未設定の場合、処理終了
+    const { userId, password, isSaved } = conf;
+    if ((userId === null || userId === undefined) || (password === null || password === undefined))
+        process.exit(0);
 
-/* メイン処理開始 */
-(async () => {
-    // webサイトにアクセス
-    let overTime = await getOverTime(userId, password);
-    if (overTime === undefined || overTime === null) return;
-    // 週次の報告資料を作成
-    let fileName = await createReport(overTime, conf);
-    if (!fileName) return;
-    //#region OSによってfs.copyFileSync（node.js v8.9.4）に失敗したため、fs-extraを使う 
-    /* ・ubuntu 16.04 LTS：失敗
-     * ・macOS High Sierra 10.13.4：成功
-     * .. いずれ解決したい */
-    //#endregion
-    // 提出用のフォルダにコピーする
-    try {
-        if (isSaved) await require('fs-extra').copy(fileName, path.join(conf.outputDir, fileName));
-    } catch (err) {
-        console.log(err);
-    }
-})();
+    (async () => {
+        // webサイトにアクセス
+        let overTime = await getOverTime(userId, password);
+        if (overTime === undefined || overTime === null) return;
+        // 週次の報告資料を作成
+        let fileName = await createReport(overTime, conf);
+        if (!fileName) return;
+        //#region OSによってfs.copyFileSync（node.js v8.9.4）に失敗したため、fs-extraを使う 
+        /* ・ubuntu 16.04 LTS：失敗
+         * ・macOS High Sierra 10.13.4：成功
+         * .. いずれ解決したい */
+        //#endregion
+        // 提出用のフォルダにコピーする
+        try {
+            if (isSaved) await require('fs-extra').copy(fileName, path.join(conf.outputDir, fileName));
+        } catch (err) {
+            console.log(err);
+        }
+    })();
+};
+
+module.exports = main;
